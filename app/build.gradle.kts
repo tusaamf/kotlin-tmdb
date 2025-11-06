db55f343b92517606700aceb673396a9fe37d3c8
tusaamf <tunt.program.04098@gmail.com>
2025-11-06 18:07:28 +0700

    DONE: Update gradle lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,16 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.dataStore)
+    implementation(libs.androidx.dataStore.preferences)
+    implementation(libs.timber)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization.json)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
